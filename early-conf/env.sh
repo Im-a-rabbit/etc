7c68d163e9ba99e33e55a1a04aa1da1c09fc0352
Rabbit <dorozhenkov9@gmail.com>
2026-07-17 13:59:58 +0300

    env: add ~/.local/bin to PATH

--- early-conf/env.sh
+++ early-conf/env.sh
@@ -2,3 +2,9 @@ export EDITOR=nvim
 export VISUAL="$EDITOR"
 export PAGER=less
 export LESS='-Fr --mouse'
+
+case ":$PATH:" in
+*":$HOME/.local/bin:"*) ;;
+*) [ -d "$HOME/.local/bin" ] && PATH="$HOME/.local/bin:$PATH" ;;
+esac
+export PATH
